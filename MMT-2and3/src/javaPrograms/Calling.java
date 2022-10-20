d165360ef9b4e7c6a80e46e0181f5ffbef7c320d
sandy0518 <sai.sandeep.906@gmail.com>
2022-10-20 18:18:08 +0530

    yes

--- MMT-2and3/src/javaPrograms/Calling.java
+++ MMT-2and3/src/javaPrograms/Calling.java
@@ -9,11 +9,10 @@ public class Calling {
 	}
 	public static void m1(String a,int b,char c) {
 		System.out.println("position differed in 3rd method");
-		System.out.println("position differed in 3rd method");
 	}
 	public static void main(String[] args) {
 		m1('a',45,"s");
 		m1(11,'a',"c");
-		
+		m1("d",12,'3');
 	}
 }
